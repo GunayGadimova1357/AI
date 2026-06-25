727b4bb9adb928cb5b385105cf5f23529f65b849
Gunay <gadimovagunay87@gmail.com>
2026-06-25 08:34:11 +0400

    push

--- AI 04/AI 10/APIs/AI 10. Price prediction WEB API/AI 10. Price prediction WEB API/obj/Debug/net9.0/AI 10. Price prediction WEB API.AssemblyInfo.cs
+++ AI 04/AI 10/APIs/AI 10. Price prediction WEB API/AI 10. Price prediction WEB API/obj/Debug/net9.0/AI 10. Price prediction WEB API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI 10. Price prediction WEB API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b0ddff3887381e0df835a09555adea2dbb14c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8525257a8462cea8693f7ce95183adf3479d05dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI 10. Price prediction WEB API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI 10. Price prediction WEB API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
